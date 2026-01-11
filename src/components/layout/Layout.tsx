fe3702c1d936a18522e0c922f3119db4445a04b1
ian.godinho <maxian.godinho@gmail.com>
2026-01-11 20:05:05 -0300

    Fixing sidebar size

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -4,14 +4,15 @@ import { Header } from './Header';
 
 export function Layout() {
   return (
-    <div className="flex min-h-screen bg-slate-50">
-      <Sidebar />
-      <div className="flex-1 flex flex-col">
+    <div className="flex min-h-screen">
+      <div className="w-64 flex-shrink-0"></div>
+      <div className="flex-1 flex flex-col min-h-screen">
         <Header />
-        <main className="flex-1 overflow-auto">
+        <main className="flex-1 p-6">
           <Outlet />
         </main>
       </div>
+      <Sidebar />
     </div>
   );
 }
