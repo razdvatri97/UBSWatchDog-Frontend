1b299ac0d9625670722478799c75e066ed628db2
Josias Vidal <josias.vidal@edu.pucrs.br>
2026-01-14 15:19:57 -0300

    Vercel blank page fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  base: '/UBSWatchDog-Frontend/',
+  base: '/',
   plugins: [react()],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
